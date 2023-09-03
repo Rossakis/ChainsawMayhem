b1bd58781c012fc7f1b0043eba350182796c22bc
Georgios Tsolakidis <rossakis@hotmail.com>
2023-09-03 23:01:29 +0300

    TriggerAreaUI can now enable Player States/Abilities

--- Assets/Scripts/UIScripts/World Space/TriggerAreaUI.cs
+++ Assets/Scripts/UIScripts/World Space/TriggerAreaUI.cs
@@ -1,6 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using ChainsawMan.PlayerClass;
+using ChainsawMan.PlayerClass.States;
+using ChainsawMan.PlayerClass.States.Grounded;
 using UnityEngine;
 
 namespace ChainsawMan
@@ -11,15 +13,40 @@ namespace ChainsawMan
         [Tooltip("Select this if this UI element should have animation, instead of a static image.")]
         [SerializeField] private bool animated;
         [SerializeField] private AnimationClip animationClip;
+
+        [Tooltip("The Player States that will be unlocked when entering this trigger area. If unassigned, nothing will happen.")]
+        [SerializeField] private List<PlayerStates> playerStatesToUnlock;
         
         private SpriteRenderer spriteRenderer;
         private Animator animator;//in case it needs to be animated
         private int animHash;
+        private PlayerController player;//reference the player who enters the trigger area, to get access to his states
 
+        public enum PlayerStates//all the states that can be disabled/activated
+        {
+            //Ground Attacks
+            GroundAttackOne,
+            GroundAttackTwo,
+            GroundAttackThree,
+            WalkingAttackOne,
+            WalkingAttackTwo,
+            KnockUp,
+            
+            //Air Attacks
+            AerialAttackOne,
+            AerialAttackTwo,
+            AerialAttackThree,
+            
+            //Universal States
+            Dash
+        }
+        
+        
         private void Start()
         {
             spriteRenderer = GetComponent<SpriteRenderer>();
             spriteRenderer.enabled = false;
+            player = GameObject.FindWithTag("Player").GetComponentInParent<PlayerController>();
             
             if (animated)
             {
@@ -34,6 +61,9 @@ namespace ChainsawMan
             if (other.CompareTag("Player"))
             {
                 spriteRenderer.enabled = true;
+
+                if (playerStatesToUnlock.Count > 0) //only activate the player state if there is at least one state in the list
+                    UnlockStates();
             }
         }
         
@@ -42,6 +72,51 @@ namespace ChainsawMan
             if (other.CompareTag("Player"))
             {
                 spriteRenderer.enabled = false;
+            }
+        }
+
+        private void UnlockStates()
+        {
+            foreach (var state in playerStatesToUnlock)//go through the list
+            {
+                switch (state)//and enable the according state
+                {
+                    //Ground Attacks
+                    case PlayerStates.GroundAttackOne:
+                        player.GroundAttackOne.isUnlocked = true;
+                        break;
+                    case PlayerStates.GroundAttackTwo:
+                        player.GroundAttackTwo.isUnlocked = true;
+                        break;
+                    case PlayerStates.GroundAttackThree:
+                        player.GroundAttackThree.isUnlocked = true;
+                        break;
+                    case PlayerStates.WalkingAttackOne:
+                        player.WalkingGroundAttackOne.isUnlocked = true;
+                        break;
+                    case PlayerStates.WalkingAttackTwo:
+                        player.WalkingGroundAttackTwo.isUnlocked = true;
+                        break;
+                    case PlayerStates.KnockUp:
+                        player.KnockUpAttack.isUnlocked = true;
+                        break;
+                    
+                    //Airborne Attacks
+                    case PlayerStates.AerialAttackOne:
+                        player.AerialAttackOne.isUnlocked = true;
+                        break;
+                    case PlayerStates.AerialAttackTwo:
+                        player.AerialAttackTwo.isUnlocked = true;
+                        break;
+                    case PlayerStates.AerialAttackThree:
+                        player.AerialAttackThree.isUnlocked = true;
+                        break;
+                    
+                    //Universal States
+                    case PlayerStates.Dash:
+                        player.Dashing.isUnlocked = true;
+                        break;
+                }
             }
         }
     }
